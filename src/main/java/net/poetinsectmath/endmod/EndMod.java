aa8829f9ba3edc70b2cc0ffc52057a1c0b8b6ad7
uncookednehuel <74217788+uncoolnehuel@users.noreply.github.com>
2022-10-18 19:45:37 -0400

    First commit prior to adding to github

---
+++ src/main/java/net/poetinsectmath/endmod/EndMod.java
@@ -0,0 +1,16 @@
+package net.poetinsectmath.endmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EndMod implements ModInitializer {
+	public static final String MOD_ID = "endmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/poetinsectmath/endmod/EndModClient.java
@@ -0,0 +1,10 @@
+package net.poetinsectmath.endmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
